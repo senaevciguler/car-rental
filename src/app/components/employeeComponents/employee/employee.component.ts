2fe81a19c98ee196af1f3f48d283e1292c54de76
senaevciguler <senaevci@gmail.com>
2020-10-10 09:54:05 +0300

    add some component and service more

--- src/app/components/employeeComponents/employee/employee.component.ts
+++ src/app/components/employeeComponents/employee/employee.component.ts
@@ -1,15 +1,101 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Employee } from 'src/app/model/employee.model';
+import { EmployeeService } from 'src/app/service/employee.service';
+import { Utility } from 'src/app/_base/utility';
+import {BaseComponent} from '../../../_base/base.component';
 
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
-  styleUrls: ['./employee.component.css']
+  styleUrls: ['./employee.component.scss']
 })
-export class EmployeeComponent implements OnInit {
+export class EmployeeComponent extends BaseComponent {
 
-  constructor() { }
+  employeeForm: FormGroup;
+  employee: Employee;
+  dataSource = [];
+  
+  constructor(
+    private route: ActivatedRoute,
+    private router:Router,
+    private employeeService:EmployeeService) {
 
-  ngOnInit(): void {
+    super();
+  }
+  
+  init() {
+    this.employeeForm = new FormGroup({
+      name: new FormControl(this.employee.name, [
+        Validators.required,
+      ]),
+      lastName: new FormControl(this.employee.lastName, [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(9)
+      ]),
+      employeeId: new FormControl(this.employee.employeeId,[
+        Validators.required
+      ])
+    });
+
+    this.isComponentReady = true;
   }
 
+  ngOnInit(): void {
+    super.ngOnInit();
+    this.route.params.subscribe(params => {
+    this.loadEmployee(params['id']);
+    });
+
+  }
+
+  ngAfterViewInit() {
+  }
+
+
+  actionSave(){
+    if (!this.employeeForm.valid) {
+      return;
+    }
+    
+    const model = this.employeeForm.value;
+    this.employee.name = model.name;
+    this.employee.lastName = model.lastName;
+    this.employee.employeeId = model.employeeId;
+    
+    
+    if (Utility.isEmpty(this.employee.id)) {
+      this.employeeService.createEmployee(this.employee).subscribe(response => {
+        console.log(response)
+        if (Utility.isSuccess(response)) {
+          this.router.navigate(['/employees']);
+        }
+      });
+    } else {
+      this.employeeService.updateEmployee(this.employee).subscribe(response => {
+        if (Utility.isSuccess(response)) {
+          this.router.navigate(['/employees']);
+        }
+      });
+    }
+  
+  }
+  
+    private loadEmployee(id: any) {
+      if (Utility.isEmpty(id)) {
+        this.employee = new Employee();
+        this.init();
+        return;
+      }
+      this.employeeService.retrieveEmployee(id).subscribe((response) => {
+        if (Utility.isSuccess(response)) {
+          this.employee = Object.assign(new Employee(), response['payload']);
+          this.init();
+        }
+      });
+  
+    }
+      
 }
